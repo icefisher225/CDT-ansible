1675a5906e90e6f8b724c409ae3f7a1523346855
icefisher225 <ryan225360@gmail.com>
2022-11-11 14:36:47 -0500

    working

--- tools/noChangesForYou.py
+++ tools/noChangesForYou.py
@@ -14,15 +14,16 @@ def inputFiles(trackedFiles: list[str])-> None:
         except:
             debug_print(f"File {fl} does not exist, please try again")
         
-        trackedFiles.append(fl)
+        trackedFiles.append(str(fl))
 
         debug_print(f"Files to be saved: {trackedFiles}")
-        done = input("Press enter to continue, or type 'done' to finish")
+        done = input("Press enter to continue, or type 'done' to finish: ")
         if done.lower() == "done":
             break
 
 def hashFile(fl: str) -> str:
-    return str(hashlib.sha256(open(fl, "rb").read()).hexdigest())
+    with open(fl, "rb") as f:
+        return hashlib.sha256(f.read()).hexdigest()
 
 def hashFiles(trackedFiles: list[str], hashes: dict[str, str]) -> None:
     for fl in trackedFiles:
@@ -32,20 +33,22 @@ def hashFiles(trackedFiles: list[str], hashes: dict[str, str]) -> None:
 def collectFiles(trackedFiles: list[str], savedFiles: dict[str, bytes]) -> None:
     for fl in trackedFiles:
         debug_print(f"Collecting {fl}")
-        savedFiles[fl] = open(fl, "rb").read()
+        with open(fl, "rb") as f:
+            savedFiles[fl] = f.read()
 
 def checkFiles(trackedFiles: list[str], hashes: dict[str, str], savedFiles: dict[str, bytes]) -> None:
     for fl in trackedFiles:
         debug_print(f"Checking {fl}")
         if hashFile(fl) != hashes[fl]:
             debug_print(f"File {fl} has changed, reverting to saved version")
-            open(fl, "wb").write(savedFiles[fl])
-    time.sleep(1)
+            with open(fl, "wb") as f:
+                f.write(savedFiles[fl])
+    time.sleep(10)
 
 def main():
-    trackedFiles = list[str]
-    hashes = dict[str, str]
-    savedFiles = dict[str, bytes]
+    trackedFiles = []
+    hashes = dict()
+    savedFiles = dict()
     inputFiles(trackedFiles)
     hashFiles(trackedFiles, hashes)
     collectFiles(trackedFiles, savedFiles)
