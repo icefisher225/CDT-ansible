782b66c2484dd8f7e04c521ff45b0949c5a39fca
Ryan Cheevers-Brown <ryan@cheevtech.com>
2022-11-11 00:24:02 -0500

    wrote second draft of tool to track file changes

--- keepSSHKeys.py
+++ keepSSHKeys.py
@@ -4,8 +4,11 @@ import os, sys, multiprocessing, threading, math, time, subprocess, hashlib
 def main():
     trackedFiles = list()
     hashes = dict()
+    savedFiles = dict()
     inputFiles(trackedFiles)
     hashFiles(trackedFiles, hashes)
+    while True:
+        checkFiles()
 
 def inputFiles(trackedFiles):
     while True:
@@ -22,12 +25,26 @@ def inputFiles(trackedFiles):
         if done.lower() == "done":
             break
 
+def hashFile(fl):
+    return hashlib.sha256(open(fl, "rb").read()).hexdigest()
+
 def hashFiles(trackedFiles, hashes):
     for fl in trackedFiles:
         print(f"Hashing {fl}")
-        hashes[fl] = hashlib.sha256(open(fl, "rb").read()).hexdigest()
+        hashes[fl] = hashFile(fl)
 
-    
+def collectFiles(trackedFiles, savedFiles):
+    for fl in trackedFiles:
+        print(f"Collecting {fl}")
+        savedFiles[fl] = open(fl, "rb").read()
+
+def checkFiles(trackedFiles, hashes, savedFiles):
+    for fl in trackedFiles:
+        print(f"Checking {fl}")
+        if hashFile(fl) != hashes[fl]:
+            print(f"File {fl} has changed, reverting to saved version")
+            open(fl, "wb").write(savedFiles[fl])
+    time.sleep(1)
 
 if __name__ == '__main__':
     main()
